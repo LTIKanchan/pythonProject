42ea23e59b7ab3dcab37f0312e321ea637523fd4
Kanchan Mangesh Pawar (Consultant) <KanchanMangesh.Pawar@GENRE.COM>
2021-08-10 09:29:24 +0000

    Removed comments

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from datetime import datetime
 
 
 def print_hi(name):
-    #name="kanchan"
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def print_hello(name):
-    #name="Bhagya"
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(name.__contains__("a"))
 
